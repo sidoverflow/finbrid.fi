51f6a9221571f12f1748e2121142160427fa50f4
Sidharth Sudarsan <sidsarun@gmail.com>
2020-09-13 16:22:46 -0400

    add gh-pages config

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ export default {
 	 ** See https://nuxtjs.org/api/configuration-target
 	 */
 	target: 'static',
+	router: {
+		base: '/finbrid-website/',
+	},
 	/*
 	 ** Headers of the page
 	 ** See https://nuxtjs.org/api/configuration-head
