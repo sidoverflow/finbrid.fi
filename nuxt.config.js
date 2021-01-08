cd6b3f263a96058454c340a7f0df03b2cf116dfd
Sidharth Sudarsan <sidsarun@gmail.com>
2021-01-07 21:28:34 -0500

    optimize images

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,13 @@ export default {
 	/*
 	 ** Nuxt.js dev-modules
 	 */
-	buildModules: ['@nuxtjs/vuetify'],
+	buildModules: [
+		'@nuxtjs/vuetify',
+		'@aceforth/nuxt-optimized-images',
+	],
+	optimizedImages: {
+		optimizeImages: true,
+	},
 	/*
 	 ** Nuxt.js modules
 	 */
