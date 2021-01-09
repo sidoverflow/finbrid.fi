28b828429d56d5cfcd42cc9fcc2fb175cd43608d
Sidharth Sudarsan <sidsarun@gmail.com>
2021-01-09 11:48:43 -0500

    change router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,9 +6,6 @@ export default {
 	 ** See https://nuxtjs.org/api/configuration-target
 	 */
 	target: 'static',
-	router: {
-		base: '/finbrid-website/',
-	},
 	/*
 	 ** Headers of the page
 	 ** See https://nuxtjs.org/api/configuration-head
